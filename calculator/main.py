8b9eeee71a13b85fe00604f84e398fb6a935b825
aps358 <as358@njit.edu>
2021-11-14 16:43:12 -0500

    Test Created

--- calculator/main.py
+++ calculator/main.py
@@ -1,36 +1,42 @@
+# Creating a Module Calculator
 class Calculator:
 
     # result set to 0 for initialization
     result = 0
 
+    # Defining the get_result function
     def get_result(self):
         # to get the latest result
         return self.result
 
+    # Defining the addition function
     def addition(self, value_a, value_b):
-        # to add the result of addition of two numbers to global result
+        # updates the result of addition of two numbers to global result
         self.result = (value_a + value_b)
 
-        # updates the latest result to the result
+        # returns the result
         return self.result
 
+    # Defining the subtraction function
     def subtraction(self, value_a, value_b):
-        # to add the result of subtraction of two numbers to global result
+        # updates the result of subtraction of two numbers to global result
         self.result = (value_a - value_b)
 
-        # updates the latest result to the result
+        # returns the result
         return self.result
 
+    # Defining the multiplication function
     def multiplication(self, value_a, value_b):
-        # to add the result of multiplication of two numbers to global result
+        # updates the result of multiplication of two numbers to global result
         self.result = (value_a * value_b)
 
-        # updates the latest result to the result
+        # returns the result
         return self.result
 
+    # Defining the division function
     def division(self, value_a, value_b):
-        # to add the result of division of two numbers to global result
+        # updates the result of division of two numbers to global result
         self.result = (value_a / value_b)
 
-        # updates the latest result to the result
+        # returns the result
         return self.result
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,25 +1,32 @@
 # Importing Calculator Class from calculator > main.py for Testing
+
 from calculator.main import Calculator
 
+# To check if calculator initial result is 0
 def test_calculator_result():
+    # Creating Calculator Object
     calc_obj = Calculator()
     assert calc_obj.get_result() == 0
 
+# To check if calculator addition result is correct
 def test_calculator_add():
     calc_obj = Calculator()
     calc_obj.addition(10, 20)
     assert calc_obj.get_result() == 30
 
+# To check if calculator subtraction result is correct
 def test_calculator_subtract():
     calc_obj = Calculator()
     calc_obj.subtraction(10, 20)
     assert calc_obj.get_result() == -10
 
+# To check if calculator multiplication result is correct
 def test_calculator_multiply():
     calc_obj = Calculator()
     calc_obj.multiplication(10, 20)
     assert calc_obj.get_result() == 200
 
+# To check if calculator division result is correct
 def test_calculator_divide():
     calc_obj = Calculator()
     calc_obj.division(20, 10)
