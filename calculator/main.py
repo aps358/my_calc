d0f054ac9e6f0a5abc863c5417116011b24c9d5e
aps358 <as358@njit.edu>
2021-11-24 09:19:18 -0500

    Part 2 - Handling Zero Division Error

--- calculator/main.py
+++ calculator/main.py
@@ -1,15 +1,15 @@
 """ Define the class Calculator """
+
+
 class Calculator:
     """ Creating a Module Calculator """
 
     # result set to 0 for initialization
     result = 0
 
-
     def get_result(self):
         """ Defining the get_result function """
         return self.result
-
 
     def addition(self, value_a, value_b):
         """ Defining the addition function """
@@ -18,14 +18,12 @@ class Calculator:
         # returns the result
         return self.result
 
-
     def subtraction(self, value_a, value_b):
         """ Defining the subtraction function """
         self.result = (value_a - value_b)
 
         # returns the result
         return self.result
-
 
     def multiplication(self, value_a, value_b):
         """ Defining the multiplication function """
@@ -34,10 +32,13 @@ class Calculator:
         # returns the result
         return self.result
 
-
     def division(self, value_a, value_b):
         """ Defining the division function """
-        self.result = (value_a / value_b)
+        try:
+            self.result = (value_a / value_b)
 
-        # returns the result
-        return self.result
+            # returns the result
+            return self.result
+
+        except ZeroDivisionError:
+            return "Cannot divide by zero !!!"
