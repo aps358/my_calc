b43a85ebe9aab23c186a25b14b4dfd0fd04d14a2
aps358 <as358@njit.edu>
2021-12-01 19:47:28 -0500

    Part 2 - Handling Zero Division Error

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -8,11 +8,13 @@ def test_calculator_result():
     calc_obj = Calculator()
     assert calc_obj.get_result() == 0
 
+
 def test_calculator_add():
     """ To check if calculator addition result is correct """
     calc_obj = Calculator()
     calc_obj.addition(10, 20)
     assert calc_obj.get_result() == 30
+
 
 def test_calculator_subtract():
     """ To check if calculator subtraction result is correct """
@@ -20,11 +22,13 @@ def test_calculator_subtract():
     calc_obj.subtraction(10, 20)
     assert calc_obj.get_result() == -10
 
+
 def test_calculator_multiply():
     """ To check if calculator multiplication result is correct """
     calc_obj = Calculator()
     calc_obj.multiplication(10, 20)
     assert calc_obj.get_result() == 200
+
 
 def test_calculator_divide():
     """ To check if calculator division result is correct"""
